f5c0c6abb513371cbab28ea188b25cf62bc721ee
Guilherme Motta <zevilpuppet@gmail.com>
2023-09-28 19:56:55 -0300

    demo-11

--- src/main/java/com/example/demo11/Demo11Application.java
+++ src/main/java/com/example/demo11/Demo11Application.java
@@ -2,6 +2,9 @@ package com.example.demo11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Demo11Application {
@@ -9,5 +12,18 @@ public class Demo11Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo11Application.class, args);
 	}
+
+  @RestController
+  @RequestMapping(value = "health")
+  public class Health {
+
+    @GetMapping
+    private String ok () {
+      return "ok";
+    }
+
+    
+  }
+
 
 }
